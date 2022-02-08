86e5900c15b54bfdbdc73add3010c6eb183dcf8f
86301 <863015749@qq.com>
2022-02-08 17:10:37 +0800

    react-redux的基本使用

---
+++ 5_src_react-redux的基本使用/redux/count_reducer.js
@@ -0,0 +1,15 @@
+import { INCREMENT,DECREMENT } from "./constant"
+
+
+const initState = 0
+export default function countReducer(preState=initState,action){
+    switch (action.type) {
+        case INCREMENT:
+            return preState+action.data
+        case DECREMENT:
+            return preState-action.data
+    
+        default:
+            return initState
+    }
+}
